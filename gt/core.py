d7d5055748faa4790d1f8cf8cb81078db752912d
Michal Harakal <michal.harakal@googlemail.com>
2024-07-14 11:59:49 +0200

    Add description field to executable annotation. Also change Executable signature so it returns multiple inputs tensors als list

--- gt/core.py
+++ gt/core.py
@@ -7,35 +7,38 @@ import torch
 
 
 # Custom annotation
-def Executable(func):
-    func.executable = True
-    return func
+def Executable(description):
+    def decorator(func):
+        func.executable = True
+        func.description = description
+        return func
+
+    return decorator
 
 
-def load_module_from_file(module_name: str, file_path: str):
+def load_module_from_file(module_name, file_path):
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
 
 
-def find_executable_functions(module) -> List[Callable]:
+def find_executable_functions(module):
     executables = []
     for attr_name in dir(module):
         attr = getattr(module, attr_name)
         if callable(attr) and hasattr(attr, 'executable'):
-            executables.append(attr)
+            executables.append((attr, attr.description))
     return executables
 
 
-def iterate_and_execute(folder_path: str) -> List[Dict[str, List[float]]]:
+def iterate_and_execute(folder_path):
     results = []
-
     for root, dirs, files in os.walk(folder_path):
         for dir_name in dirs:
             if dir_name.startswith("TS-"):
                 ts_number = dir_name.split('-')[1]
-                ts_path = os.path.join(root, dir_name)
+                ts_path = str(os.path.join(root, dir_name))
                 for file_name in os.listdir(ts_path):
                     if file_name.startswith("UC-") and file_name.endswith(".py"):
                         uc_number = file_name.split('-')[1].split('.')[0]
@@ -43,18 +46,18 @@ def iterate_and_execute(folder_path: str) -> List[Dict[str, List[float]]]:
                         module_name = f"{ts_number}_UC_{uc_number}"
                         module = load_module_from_file(module_name, file_path)
                         executable_functions = find_executable_functions(module)
-                        for func in executable_functions:
-                            input_value, result = func()
+                        for func, description in executable_functions:
+                            inputs, result = func()
                             results.append({
                                 'name': f"{module_name}.{func.__name__}",
-                                'input': input_value,
-                                'result': result
+                                'description': description,
+                                'inputs': inputs,
+                                'result': result,
+                                'test_suite': f"TS-{ts_number}",
+                                'use_case': f"UC-{uc_number}"
                             })
-
     return results
 
-
-# Example usage
 
 if __name__ == "__main__":
     scr_results = iterate_and_execute("/Users/A9973957/projects/tribit.ai/gradienttracer/gt/examples")
--- gt/examples/TS-0001/UC-0001.py
+++ gt/examples/TS-0001/UC-0001.py
@@ -3,8 +3,9 @@
 from core import Executable
 import torch
 
-@Executable
-def example_function():
+
+@Executable('simple expression with relu')
+def simple_expression_with_relu():
     x = torch.Tensor([-4.0]).double()
     x.requires_grad = True
     z = 2 * x + 2 + x
@@ -12,4 +13,13 @@ def example_function():
     h = (z * z).relu()
     y = h + q + q * x
     y.backward()
-    return x, y
+    return [x], y
+
+
+@Executable("simple multiplication")
+def simple_multiplication():
+    a = torch.tensor(3.0, requires_grad=True)
+    b = torch.tensor(2.0, requires_grad=True)
+    c = a * b
+    c.backward()
+    return [a, b], c
